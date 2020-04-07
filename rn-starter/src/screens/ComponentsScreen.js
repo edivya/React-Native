40961c6f539a860b60ebac7fde277710f8c9492a
edivya <enjamuridivya@gmail.com>
2020-04-07 16:01:27 -0700

    adding ListScreen

--- rn-starter/src/screens/ComponentsScreen.js
+++ rn-starter/src/screens/ComponentsScreen.js
@@ -2,18 +2,21 @@ import React from "react";
 import { Text, StyleSheet, View } from "react-native";
 
 const ComponentsScreen = () => {
-  const greetings = <Text>Hello to you</Text>;
+  const name = "Divya";
   return (
     <View>
-      <Text style={styles.textStyle}> This is the Components Screen</Text>;
-      {greetings}
+      <Text style={styles.textStyle}>Getting started with React native</Text>
+      <Text style={styles.subHeaderStyle}>My name is {name}</Text>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   textStyle: {
-    fontSize: 30
+    fontSize: 45
+  },
+  subHeaderStyle: {
+    fontSize: 20
   }
 });
 
